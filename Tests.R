5c95aaa6b3a18ede9633e2aee7d4417b4cb4d531
WITHINGTON Anna <awithin@deq.state.or.us>
2019-02-06 13:23:37 -0800

    double checking and revising table creation and merging

--- Tests.R
+++ Tests.R
@@ -226,3 +226,113 @@ n <- nn %>%
 #   mutate(`2015 Impact` = round(`2015 Weight`*`Impact Factor`),
 #          `New Impact` = round(`New Weight`*`Impact Factor`))
 n
+
+
+# 2/6/2019 ----------------------------------------------------------------
+
+m1 <- read_csv("imfoAppMassProfiles.csv")
+mass <- m1 %>%
+  mutate(`Umbrella Disposition` = ifelse(disposition %in% "landfilling", "Disposal", "Recovery")) %>% 
+  mutate(Material = recode(material, "FoodWaste" = "Food Waste")) %>% 
+  mutate(`Life Cycle Stage` = ifelse(LCstage %in% "endOfLifeTransport", "EOL Transport", "EOL")) %>% 
+  filter(`Life Cycle Stage` != "EOL Transport") %>%
+  mutate(`2015 Weight` = round(tons, digits = -2)) %>% 
+  rename(Wasteshed = wasteshed, Disposition = disposition) %>% 
+  select(Wasteshed, Material, Disposition, `Life Cycle Stage`, `Umbrella Disposition`, `2015 Weight`)
+
+
+I <- read_csv("imfoAppImpactFactors.csv")
+
+I1 <- I %>% 
+  mutate(Material = recode(material, "FoodWaste" = "Food Waste")) %>% 
+  mutate(`Life Cycle Stage` = ifelse(LCstage %in% "endOfLifeTransport", "EOL Transport", 
+                                     ifelse(LCstage %in% "endOfLife", "EOL",
+                                            ifelse(LCstage %in% "production", "Production",
+                                                   "other")))
+  ) %>% 
+  rename(Disposition = disposition, `Impact Category` = impactCategory,
+         `Impact Units` = impactUnits, `Impact Factor` = impactFactor,
+         `Implied Miles` = impliedMiles) %>% 
+  select(Material, Disposition, `Life Cycle Stage`, `Impact Category`,
+         `Impact Units`, `Impact Factor`, `Implied Miles`)
+
+newnew_all <- mass %>%
+  mutate(`New Weight` = `2015 Weight` * 0.75)
+
+# meltedusermass <- newnew_all %>% 
+#   select(-c(Wasteshed, `Umbrella Disposition`)) %>% 
+#   melt(id.vars = c('Material', 'Disposition'))
+
+nT <- newnew_all %>% 
+  mutate(`Life Cycle Stage` = "EOL Transport")
+
+nP <- newnew_all %>% 
+  mutate(`Life Cycle Stage` = "Production",
+         Disposition = "production")
+
+nn <- newnew_all %>% 
+  rbind(nT) %>% 
+  rbind(nP)
+
+n <- nn %>% 
+  left_join(I1, by = c("Material", "Disposition",
+                       "Life Cycle Stage")) %>% 
+  mutate(`2015 Impact` = round(`2015 Weight`*`Impact Factor`),
+         `New Impact` = round(`New Weight`*`Impact Factor`))
+
+###########
+# newimpacts <- newnew_all %>% 
+#   left_join(I1, by = c("Material", "Disposition",
+#                        "Life Cycle Stage")) %>% 
+#   mutate(`2015 Impact` = round(`2015 Weight`*`Impact Factor`),
+#          `New Impact` = round(`New Weight`*`Impact Factor`))
+
+newimpactstest <- newnew %>% 
+  left_join(I1, by = c("Material", "Disposition")) %>% 
+  mutate(`2015 Impact` = round(`2015 Weight`*`Impact Factor`),
+         `New Impact` = round(`New Weight`*`Impact Factor`))
+
+meltedimpacts <- newimpacts %>% 
+  select(Material, Disposition, `Impact Category`, old_impact, new_impact) %>%
+  melt(id.vars = c('Material', 'Disposition', `Impact Category`))
+meltedimpacts <- newimpacts %>% 
+  select(Material, Disposition, `Impact Category`, old_impact, new_impact) %>%
+  melt(id.vars = c('Material', 'Disposition'))
+
+meltedimpacts <- newimpacts %>% 
+  select(-c(`2015 Weight`, `New Weight`, `Impact Factor`)) %>% 
+  gather(id.vars = c('Material', 'Disposition', `Impact Factor`, `Impact Units`)) %>% 
+  filter(!is.na(`Impact Factor`))
+
+
+nT <- newnew %>% 
+  select(-(`Life Cycle Stage`)) %>% 
+  mutate(`Life Cycle Stage` = "EOL Transport")
+
+nP <- newnew %>% 
+  select(-`Life Cycle Stage`, Disposition) %>% 
+  mutate(`Life Cycle Stage` = "Production",
+         Disposition = "production")
+
+
+
+
+nn <- newnew %>% 
+  rbind(nT) %>% 
+  rbind(nP)
+
+n <- nn %>% 
+  left_join(I1, by = c("Material", "Disposition",
+                       "Life Cycle Stage")) %>% 
+  mutate(`2015 Impact` = round(`2015 Weight`*`Impact Factor`),
+         `New Impact` = round(`New Weight`*`Impact Factor`))
+
+# %>% 
+#   mutate(`2015 Impact` = round(`2015 Weight`*`Impact Factor`),
+#          `New Impact` = round(`New Weight`*`Impact Factor`))
+n
+
+
+t <- n %>% 
+  select(Material, Disposition, `Life Cycle Stage`, `Umbrella Disposition`, `Impact Category`, `Impact Units`, `2015 Impact`, `New Impact`) %>% 
+  gather(key = "Scenario", value = "Impact", -c(Material, Disposition, `Life Cycle Stage`, `Umbrella Disposition`, `Impact Category`, `Impact Units`))
