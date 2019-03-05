d768231ec7b2248f67a6ea90a86bafc23028431d
WITHINGTON Anna <awithin@deq.state.or.us>
2019-03-04 17:08:23 -0800

    Updates to material names

--- Data_prep.R
+++ Data_prep.R
@@ -20,27 +20,45 @@ library(shinyBS)
 m1 <- read_csv("imfoAppMassProfiles.csv")
 mass <- m1 %>%
   mutate(`Umbrella Disposition` = ifelse(disposition %in% "landfilling", "Disposal", "Recovery")) %>% 
-  mutate(Material = recode(material, "FoodWaste" = "Food Waste")) %>% 
+  mutate(Material = recode(material, "FoodWaste" = "Food",
+                           "Cardboard/Kraft" = "Cardboard",
+                           "Carpeting-used" = "Carpet",
+                           "Glass Containers" = "Glass",
+                           "Nonrecyclables" = "Trash",
+                           "Paper Fiber" = "Paper",
+                           "Rigid Plastic Cont." = "Rigid Plastic",
+                           "Scrap Metal - Other" = "Scrap Metal",
+                           "Wood Waste" = "Wood")) %>% 
   mutate(`Life Cycle Stage` = ifelse(LCstage %in% "endOfLifeTransport", "EOL Transport", "EOL")) %>% 
   filter(`Life Cycle Stage` != "EOL Transport") %>%
   mutate(`2015 Weight` = round(tons, digits = -2)) %>% 
-  rename(Wasteshed = wasteshed, Disposition = disposition) %>% 
+  rename(Wasteshed = wasteshed,
+         Disposition = disposition) %>% 
   select(Wasteshed, Material, Disposition, `Life Cycle Stage`, `Umbrella Disposition`, `2015 Weight`)
 write_csv(mass, "mass.csv")
 
 I <- read_csv("imfoAppImpactFactors.csv")
 
 I1 <- I %>% 
-  mutate(Material = recode(material, "FoodWaste" = "Food Waste")) %>% 
+  mutate(Material = recode(material, "FoodWaste" = "Food",
+                           "Cardboard/Kraft" = "Cardboard",
+                           "Carpeting-used" = "Carpet",
+                           "Glass Containers" = "Glass",
+                           "Nonrecyclables" = "Trash",
+                           "Paper Fiber" = "Paper",
+                           "Rigid Plastic Cont." = "Rigid Plastic",
+                           "Scrap Metal - Other" = "Scrap Metal",
+                           "Wood Waste" = "Wood")) %>% 
   mutate(`Life Cycle Stage` = ifelse(LCstage %in% "endOfLifeTransport", "EOL Transport", 
                                      ifelse(LCstage %in% "endOfLife", "EOL",
                                             ifelse(LCstage %in% "production", "Production",
                                                    "other")))
   ) %>% 
-  rename(Disposition = disposition, `Impact Category` = impactCategory,
-         `Impact Units` = impactUnits, `Impact Factor` = impactFactor,
+  rename(Disposition = disposition, Category = impactCategory,
+         Units = impactUnits, Factor = impactFactor,
          `Implied Miles` = impliedMiles) %>% 
-  select(Material, Disposition, `Life Cycle Stage`, `Impact Category`,
-         `Impact Units`, `Impact Factor`, `Implied Miles`)
+  mutate(Category = recode(Category, Eutrophication = "Excess nutrients")) %>% 
+  select(Material, Disposition, `Life Cycle Stage`, Category,
+         Units, Factor, `Implied Miles`)
 
 write_csv(I1, "I1.csv")
