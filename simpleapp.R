b71cf8b66abf5390454e03ab86ed47dccdacaefe
WITHINGTON Anna <awithin@deq.state.or.us>
2019-03-08 17:03:18 -0800

    added region selector

--- simpleapp.R
+++ simpleapp.R
@@ -35,23 +35,23 @@ ui <- fluidPage(
   navbarPage("Lifecycle Impacts Tool",
              
              # Introduction tab -------------------------------------------------------
-             # tabPanel("Introduction",
-             #          fluidPage(
-             #            column(12,
-             #                   align = "center",
-             #                   h3("Welcome to the Lifecycle Impacts Tool!", align = "center"),
-             #                   # h2("Interactive Visualizer!", align = "center"),
-             #                   # div(img(src = 'greenpic.jpeg', height="50%", width="50%"), style = "text-align: center;"),
-             #                   br(),
-             #                   HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/-9JRowyICbo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'),
-             #                   br(),
-             #                   br(),
-             #                   div( style = "width:500px; text-align:center;",
-             #                        p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-             #                   )
-             #            )
-             #          )
-             # ),
+             tabPanel("Introduction",
+                      fluidPage(
+                        column(12,
+                               align = "center",
+                               h3("Welcome to the Lifecycle Impacts Tool!", align = "center"),
+                               # h2("Interactive Visualizer!", align = "center"),
+                               # div(img(src = 'greenpic.jpeg', height="50%", width="50%"), style = "text-align: center;"),
+                               br(),
+                               HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/-9JRowyICbo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'),
+                               br(),
+                               br(),
+                               div( style = "width:500px; text-align:center;",
+                                    p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                               )
+                        )
+                      )
+             ),
 
 # Visualize Tab -----------------------------------------------------------
 
@@ -90,7 +90,12 @@ tabPanel("Visualize!",
                   )
                   )),
            column(4,
-                  wellPanel()),
+                  wellPanel(
+                    selectInput(inputId = "userregion",
+                                label = "",
+                                choices = unique(mass$wasteshed),
+                                selected = "Oregon total")
+                  )),
            column(4,
                   wellPanel(),
                   wellPanel(
@@ -101,6 +106,7 @@ tabPanel("Visualize!",
            )
          )
                       ),
+tabPanel("Context"),
 
 # More NavbarMenu ---------------------------------------------------------
 
