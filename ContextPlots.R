2393470bb9ca4a0d9f89bbf5caa9adbe541ae413
WITHINGTON Anna <awithin@deq.state.or.us>
2019-03-22 10:51:58 -0700

    updates to context plot drafts, including separate y axes

--- ContextPlots.R
+++ ContextPlots.R
@@ -4,33 +4,61 @@ library(tidyr)
 
 tr <- readRDS("impacts_by_LC_stage_data.RData")
 
-Bakertr <- tr %>% 
+regionalcontextb <- tr %>% 
   filter(wasteshed == "Baker")
 
-p <- plot_ly(data = Bakertr,
+
+
+p <- plot_ly(data = regionalcontextb %>% filter(umbDisp == "Net"),
        x = ~appMaterial,
        y = ~impact,
-       type = "bar") %>% 
-  add_trace(y = ~tons, yaxis = "y2") %>% 
+       type = "bar") %>%
+  add_trace(y = ~tons, yaxis = "y2") %>%
   layout(barmode = "group",
          yaxis2 = list(overlaying = "y",
                        side = "right"))
 p
 
-Animals <- c("giraffes", "orangutans", "monkeys")
-SF_Zoo <- c(20, 14, 23)
-LA_Zoo <- c(12, 18, 29)
-data <- data.frame(Animals, SF_Zoo, LA_Zoo)
+rcn <- regionalcontextb %>%
+  filter(umbDisp == "Net")
+p <- plot_ly(data = rcn,
+             x = ~appMaterial,
+             y = ~impact,
+             type = "bar") %>%
+  add_trace(y = ~tons, yaxis = "y2") %>%
+  layout(barmode = "group",
+         yaxis2 = list(overlaying = "y",
+                       side = "right"))
+p
+
+
+
+
+p <- plot_ly(data = regionalcontextb %>% filter(umbDisp == "Net"),
+       x = ~appMaterial,
+       y = ~impact,
+       type = "bar") %>%
+  add_trace(y = ~tons) %>%
+  layout(barmode = "group")
+p
+
+
+
+#look at data structure for stacked bar
+# Animals <- c("giraffes", "orangutans", "monkeys")
+# SF_Zoo <- c(20, 14, 23)
+# LA_Zoo <- c(12, 18, 29)
+# data <- data.frame(Animals, SF_Zoo, LA_Zoo)
 
 #create stacked bar
 
-Bakertrimpact <- Bakertr %>% 
+regionalcontextbimpact <- regionalcontextb %>% 
   filter(umbDisp != "Net") %>% 
   select(wasteshed, appMaterial, umbDisp, impact, tons) %>% 
   spread(key = umbDisp, value = impact)
 
 
-q <- plot_ly(data = Bakertrimpact,
+q <- plot_ly(data = regionalcontextbimpact,
              x = ~appMaterial,
              y = ~Disposal,
              name = "Disposal",
@@ -43,7 +71,8 @@ q <- plot_ly(data = Bakertrimpact,
          
 q
 
-pp <- plot_ly(data = Bakertrimpact,
+# change x and y axes to create horizontal stacked bar
+pp <- plot_ly(data = regionalcontextbimpact,
              y = ~appMaterial,
              x = ~Disposal,
              name = "Disposal",
@@ -56,13 +85,14 @@ pp <- plot_ly(data = Bakertrimpact,
 
 pp
 
-Bakertrtons <- Bakertr %>% 
+#tons, divided by disposal and recovery
+regionalcontextbtons <- regionalcontextb %>% 
   filter(umbDisp != "Net") %>%
   select(wasteshed, appMaterial, umbDisp, impact, tons) %>% 
   filter(umbDisp != "Production") %>% 
   spread(key = umbDisp, value = tons)
 
-tt <- plot_ly(data = Bakertrtons,
+tt <- plot_ly(data = regionalcontextbtons,
               y = ~appMaterial,
               x = ~Disposal,
               name = "Disposal",
@@ -77,22 +107,29 @@ sub <- subplot(pp, tt, shareY = TRUE)
 sub
 
 # Net impact
-Bakertrnet <- Bakertr %>% 
+regionalcontextbnet <- regionalcontextb %>% 
   filter(umbDisp == "Net") %>% 
   select(wasteshed, appMaterial, umbDisp, impact, tons)
 
-bb <- plot_ly(data = Bakertrnet,
+
+tons <- plot_ly(data = regionalcontextbtons,
               y = ~appMaterial,
-              x = ~impact,
-              name = "Net Impact",
+              x = ~tons,
+              name = "Waste, in tons",
               type = "bar") %>%
   layout(barmode = "relative")
-bb
 
-sub2 <- subplot(tt, bb)
+impact <- plot_ly(data = regionalcontextbnet,
+              y = ~appMaterial,
+              x = ~impact,
+              name = "Net Global Warming Impact",
+              type = "bar") %>%
+  layout(barmode = "relative")
+
+sub2 <- subplot(tons, impact)
 sub2
 
-scatter <- plot_ly(data = Bakertrnet,
+scatter <- plot_ly(data = regionalcontextbnet,
         x = ~tons,
         y = ~impact,
         text = ~appMaterial,
