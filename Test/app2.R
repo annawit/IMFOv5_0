59e3a510d5f0431927754fe6af3a71b8dd1a2d13
WITHINGTON Anna <awithin@deq.state.or.us>
2019-04-12 11:33:04 -0700

    Example of server-side user data entry with DT datatable

--- Test/app2.R
+++ Test/app2.R
@@ -6,12 +6,18 @@ mass <- read_csv("mass.csv")
 shinyApp(
   ui = fluidPage(
     DTOutput('x1'),
-    textOutput("text")
+    textOutput("text"),
+    textOutput("text2")
   ),
   
   
   server = function(input, output, session) {
-    x <- mass
+    x <- data.frame(mass)
+    
+    y <- reactive({
+      input$x1_cell_edit
+      x
+    })
     
     x$Date <- Sys.time() + seq_len(nrow(x))
     
@@ -33,5 +39,11 @@ shinyApp(
       input$x1_cell_edit
       x[1, 6]
     })
+    
+    output$text2 <- renderText({
+      input$x1_cell_edit
+      y()[1, 6]
+    })
+    
   }
 )
